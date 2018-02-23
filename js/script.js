0b0a3d53b6062f49946b740ce88a2c87b669ae7f
Adam Pawliński <adam.pawlinski@gmail.com>
2018-02-23 22:40:57 +0100

    add script and text

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var a = prompt("enter first digit");
+var b = prompt("enter second digit");
+var value = (a * a) - (2 * a * b) - (b * b);
+alert("(a * a) - (2 * a * b) - (b * b) is eqal to: " + value);
+console.log(value);
+if (value == 0) {
+  alert("result is equal 0");
+  console.log("result is equal 0");
+}
+else if (value <= 0){
+  alert("negative result");
+  console.log("negative result");
+}
+else {
+  alert("positive result");
+  console.log("positive result");
+}
